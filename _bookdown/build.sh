2365e08d24901e914e2002f1fa4f1f6e569c3d93
Paul M. Jones <pmjones88@gmail.com>
2017-04-23 11:45:58 -0500

    update with logo

--- _bookdown/build.sh
+++ _bookdown/build.sh
@@ -1,4 +1,4 @@
 export CSS_BOOTSWATCH=lumen
 export CSS_PRISM=ghcolors
-# export MENU_LOGO=https://...
+export MENU_LOGO=https://raw.githubusercontent.com/atlasphp/atlasphp.github.io/master/images/atlas.png
 ./vendor/bin/bookdown ./_bookdown.json
