5cbbdde555a6ceb80ca835c518b58b7043fe85f6
Paul M. Jones <pmjones88@gmail.com>
2017-05-22 09:48:30 -0500

    update docs

--- _bookdown/build.sh
+++ _bookdown/build.sh
@@ -1,4 +1,4 @@
-export CSS_BOOTSWATCH=lumen
-export CSS_PRISM=ghcolors
+export CSS_BOOTSWATCH=yeti
+export CSS_PRISM=base16-ateliersulphurpool.light
 export MENU_LOGO=https://raw.githubusercontent.com/atlasphp/atlasphp.github.io/master/images/atlas.png
 ./vendor/bin/bookdown ./_bookdown.json
